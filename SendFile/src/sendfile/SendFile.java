815ed1ed6c8bb433be0b98d8e87c1dc062707778
Bill Carson <amolisporcatus@gmail.com>
2011-03-16 20:59:12 -0500

    added source

---
+++ SendFile/src/sendfile/SendFile.java
@@ -0,0 +1,21 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package sendfile;
+
+/**
+ *
+ * @author anolis
+ */
+public class SendFile {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+
+}
---
+++ SendFileS/src/sendfile/DownloadFile.java
@@ -0,0 +1,14 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package sendfile;
+
+/**
+ *
+ * @author anolis
+ */
+public class DownloadFile {
+
+}
